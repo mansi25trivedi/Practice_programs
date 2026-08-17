fc6f5d63011349f1adf56008ce6bb85e29595adf
mansi25trivedi <trivedi.25mansi@gmail.com>
2026-08-17 20:38:55 +0530

    armstrong number

--- loops.cpp
+++ loops.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 int main(){
     //Print the reverse of a given number
@@ -31,6 +32,29 @@ int main(){
     }
 
     // Check if a number is an Armstrong number.
+    int n2;
+    cout<<"enter any number: ";
+    cin>>n2;
+    n=n2;
+    int oldn = n2;
+    int c1 = 0;
+    while(n!=0){
+        int r = n%10;
+        n/=10;
+        c1++;
+    }
+    int a2 = 0;
+    while(n2!=0){
+        int r = n2%10;
+        a2 = a2*10 + pow(r , c1);
+        n2/=10;
+    }
+    if(oldn == a2){
+        cout<<"the given number is an Armstrong number";
+    }else{
+    cout<<"the given number is not an Armstrong number";
+    }
+
 
     return 0;
 }
